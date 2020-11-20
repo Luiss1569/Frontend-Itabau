b47a5d3723e6120224c2139f0d1d54be2155f797
luiss1569 <rcsrcs1569@gmail.com>
2020-11-20 12:53:49 -0300

    up

--- src/pages/Painel/views/tables/matriz.js
+++ src/pages/Painel/views/tables/matriz.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { Table } from 'antd';
 
+function convert(date) {
+
+    var nomeMeses = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEC']
+    var data = new Date(date);
+    var dia = data.getDate();
+    var mes = data.getMonth();
+    mes = nomeMeses[mes];
+    var ano = data.getFullYear();
+
+    return `${data.getHours()}h${data.getMinutes()} ${[dia, mes, ano].join('/')}`
+
+}
+
 const columns = [
-    { title: 'Id', dataIndex: 'ID', key: 'ID',responsive: ['md'], width: '4%', sorter: (a, b) => a.ID - b.ID,
-    sortDirections: ['ascend'], },
-    { title: 'Nome', dataIndex: 'NOME', key: 'NOME', width: '15%'},
-    { title: 'Email', dataIndex: 'EMAIL', key: 'EMAIL',responsive: ['md'], width: '15%' },
-    { title: 'Telefone', dataIndex: 'TELEFONE', key: 'TELEFONE',responsive: ['md'], width: '10%' },
-    { title: 'Cidade', dataIndex: 'CIDADE', key: 'CIDADE',responsive: ['md'], width: '13%',  sorter: (a,b) => a>b?-1:1 },
-    { title: 'Estado', dataIndex: 'ESTADO', key: 'ESTADO',responsive: ['md'], width: '9%'},
-    { title: 'Assuto', dataIndex: 'ASSUNTO', key: 'ASSUNTO', width: '10%'},
-    { title: 'Mensagem', dataIndex: 'MENSAGEM', key: 'MENSAGEM',ellipsis: {showTitle: false},responsive: ['md'], width: '7%' },
-    { title: 'DATA/TIME', dataIndex: 'DATATIME', key: 'DATATIME' },
+    {
+        title: 'Id', dataIndex: 'ID', key: 'ID', responsive: ['md'], width: '4%', sorter: (a, b) => a.ID - b.ID,
+        sortDirections: ['ascend'],
+    },
+    { title: 'Nome', dataIndex: 'NOME', key: 'NOME', width: '15%' },
+    { title: 'Email', dataIndex: 'EMAIL', key: 'EMAIL', responsive: ['md'], width: '15%' },
+    { title: 'Telefone', dataIndex: 'TELEFONE', key: 'TELEFONE', responsive: ['md'], width: '10%' },
+    { title: 'Cidade', dataIndex: 'CIDADE', key: 'CIDADE', responsive: ['md'], width: '13%', sorter: (a, b) => a > b ? -1 : 1 },
+    { title: 'Estado', dataIndex: 'ESTADO', key: 'ESTADO', responsive: ['md'], width: '9%' },
+    { title: 'Assuto', dataIndex: 'ASSUNTO', key: 'ASSUNTO', width: '10%' },
+    { title: 'Mensagem', dataIndex: 'MENSAGEM', key: 'MENSAGEM', ellipsis: { showTitle: false }, responsive: ['md'], width: '7%' },
+    { title: 'DATA/TIME', dataIndex: 'DATATIME', key: 'DATATIME', render: convert },
 ];
 
 export default function TablePesq(props) {
@@ -21,12 +36,12 @@ export default function TablePesq(props) {
             showHeader bordered pagination={false}
             expandable={{
                 expandedRowRender: record => (
-               <>
-                <p style={{ margin: 0 }}><label>Mensagem:</label>{record.MENSAGEM}</p>
-               </>
+                    <>
+                        <p style={{ margin: 0 }}><label>Mensagem:</label>{record.MENSAGEM}</p>
+                    </>
                 ),
             }}
-            dataSource={props.data.map((pesq)=>({
+            dataSource={props.data.map((pesq) => ({
                 key: pesq.ID,
                 ...pesq
             }))}
--- src/pages/Painel/views/tables/pesquisa.js
+++ src/pages/Painel/views/tables/pesquisa.js
@@ -1,5 +1,18 @@
 import React from 'react'
-import { Table } from 'antd';
+import { Table, Tooltip } from 'antd';
+
+function convert(date) {
+
+    var nomeMeses = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEC']
+    var data = new Date(date);
+    var dia = data.getDate();
+    var mes = data.getMonth();
+    mes = nomeMeses[mes];
+    var ano = data.getFullYear();
+
+    return `${data.getHours()}h${data.getMinutes()} ${[dia, mes, ano].join('/')}`
+
+}
 
 const columns = [
     { title: 'Id', dataIndex: 'ID', key: 'ID',responsive: ['md'],sorter: (a, b) => a.ID - b.ID,
@@ -10,7 +23,7 @@ const columns = [
     { title: 'Telefone', dataIndex: 'TELEFONE', key: 'TELEFONE',responsive: ['md'] },
     { title: 'Cidade', dataIndex: 'CIDADE', key: 'CIDADE',responsive: ['md'],  sorter: (a,b) => a>b?-1:1 },
     { title: 'Estado', dataIndex: 'ESTADO', key: 'ESTADO',responsive: ['md'] },
-    { title: 'DATA/TIME', dataIndex: 'DATATIME', key: 'DATATIME' },
+    { title: 'DATA/TIME', dataIndex: 'DATATIME', key: 'DATATIME', render:convert },
 ];
 
 export default function TablePesq(props) {
@@ -21,13 +34,13 @@ export default function TablePesq(props) {
             expandable={{
                 expandedRowRender: record => (
                <>
-                <p style={{ margin: 0 }}><label>O1:</label>{record.O1}</p>
-                <p style={{ margin: 0 }}><label>O2:</label>{record.O2}</p>
-                <p style={{ margin: 0 }}><label>O3:</label>{record.O3}</p>
-                <p style={{ margin: 0 }}><label>O4:</label>{record.O4}</p>
-                <p style={{ margin: 0 }}><label>O5:</label>{record.O5}</p>
-                <p style={{ margin: 0 }}><label>O6:</label>{record.O6}</p>
-                <p style={{ margin: 0 }}><label>O7:</label>{record.O7}</p>
+                <p style={{ margin: 0 }}><Tooltip title={'O que você acha dos produtos Itabaú e suas variedades?'}><label>O1:</label>{record.O1}</Tooltip></p>
+                <p style={{ margin: 0 }}><Tooltip title={'Se você pudesse melhorar algo em nosso produtos, o que melhoraria? '}><label>O2:</label>{record.O2}</Tooltip></p>
+                <p style={{ margin: 0 }}><Tooltip title={'Qual o sabor que falta de picolé e sorvete de massa em nossa linha, que você acha interessante termos?'}><label>O3:</label>{record.O3}</Tooltip></p>
+                <p style={{ margin: 0 }}><Tooltip title={'O que você acha do preço dos nossos produto?'}><label>O4:</label>{record.O4}</Tooltip></p>
+                <p style={{ margin: 0 }}><Tooltip title={'Você recomendaria a Itabaú para seus parentes, amigos ou companheiros?'}><label>O5:</label>{record.O5}</Tooltip></p>
+                <p style={{ margin: 0 }}><Tooltip title={'Com qual frequência você toma sorvete?'}><label>O6:</label>{record.O6}</Tooltip></p>
+                <p style={{ margin: 0 }}><Tooltip title={'Por favor, diga-nos o que podemos fazer para melhorar? '}><label>O7:</label>{record.O7}</Tooltip></p>
                </>
                 ),
             }}
