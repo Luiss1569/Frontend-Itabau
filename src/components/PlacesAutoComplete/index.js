9d6d2fc9c88edcad9c0398feba90e94732a6abe3
Luis Ricardo Santos <rcsrcs1569@gmail.com>
2021-10-29 14:05:34 +0000

    ajustes onde encontrar

--- src/components/PlacesAutoComplete/index.js
+++ src/components/PlacesAutoComplete/index.js
@@ -12,6 +12,7 @@ import "@reach/combobox/styles.css"
 import './style.css'
 import { useState } from "react";
 import { useEffect } from "react";
+import axios from "axios";
 
 function PlacesAutocomplete(props) {
     const [value, setValue] = useState("");
@@ -28,11 +29,13 @@ function PlacesAutocomplete(props) {
                 return response.json();
             }).catch(function (error) {
                 console.log('There has been a problem with your fetch operation: ' + error.message);
-                return []
+                return {
+                    data: true
+                }
             });
 
             if(response.data){
-                setData([])
+                getLocationOutherAPI()
                 return;
             }
 
@@ -43,15 +46,53 @@ function PlacesAutocomplete(props) {
             })
             setData(citys)
         }
-        getLocation()
+        getLocation();
         
-    }, [value])
+    }, [value]); // eslint-disable-next-line
+
+    async function getLocationOutherAPI(){
+        const response = await axios
+            .get(`https://nominatim.openstreetmap.org/search?format=json&city=${value}&&limit=5`)
+            .then(({data}) => data)
+            .catch(e=> console.log(e.message));
+
+        const citys = response.map(city=>{
+            return { 
+                LATITUDE: city.lat, 
+                LONGITUDE: city.lon,
+                description: city.display_name.split(",")[0]
+            }
+        })
+
+        const reduced =[]
+        citys.forEach((item) => {
+            var duplicated  = reduced.findIndex(redItem => {
+                return item.a === redItem.a;
+            }) > -1;
+        
+            if(!duplicated) {
+                reduced.push(item);
+            }
+        });
+
+        setData(reduced)
+    }
 
     const handleInput = (e) => {
         setValue(e.target.value);
     };
 
     const handleSelect = (val) => {
+        console.log(data)
+        if(data[0].LATITUDE && data[0].LONGITUDE){
+            const selected = data.filter(as => as.description === val)
+            props.setLat(selected[0].LATITUDE)
+            props.setLong(selected[0].LONGITUDE)
+            props.setLocation(val)
+
+            return;
+        }
+
         async function getLocation(){
             const response = await fetch(`${api}/php/cliente/like_city.php?name=${val}`, {
                 method: 'get'
@@ -76,7 +117,9 @@ function PlacesAutocomplete(props) {
     return (
         <div classname='combobox-responsive'>
             <Combobox onSelect={handleSelect} aria-labelledby="demo">
-            <ComboboxInput value={value} onChange={handleInput} placeholder={"Digite sua cidade, estado ou pais ..."} disabled={false} style={{ borderColor: '#135749', fontFamily: "Philosopher", width: '100%', height: 30, fontSize: 20, borderRadius: 10, textAlign: 'center'}} />
+            <ComboboxInput value={value} onChange={handleInput} placeholder={"Digite sua cidade, estado ou pais ..."} disabled={false} 
+                style={{ borderColor: '#135749', fontFamily: "Philosopher", width: '100%', height: 30, fontSize: 20, borderRadius: 10, textAlign: 'center'}} 
+            />
             <ComboboxPopover>
                 <ComboboxList>
                     {data.length !== 0 &&
